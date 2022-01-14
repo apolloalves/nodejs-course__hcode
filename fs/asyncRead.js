335498610a49b7ed8482d26acd6f0c4f3c6a3f7c
APOLLO ALVES <apolloapolloalves@gmail.com>
2022-01-14 19:06:13 -0300

    readFile API

--- fs/asyncRead.js
+++ fs/asyncRead.js
@@ -1,7 +1,14 @@
 const fs = require('fs').promises
 
-
 async function read() {
-    const data = await fs.readFile( 'texto.txt', 'binary' )
-    return new Buffer.from (data )
+    const data = await fs.readFile( 'text.txt', 'binary' )
+    return new Buffer.from ( data )
+}
+
+try{
+    read().then(data => console.log( data.toString()))
+
+} catch (err){
+    
+    console.log(err)
 }
---
+++ fs/html.js
@@ -0,0 +1,29 @@
+const opn = require( 'opn' )
+const http = require( 'http' )
+const { readFile } = require('fs')
+
+const host = '127.0.0.1'
+const port = '3000'
+const url = `http://${host}:${port}`
+
+
+let conteudo = ''
+
+readFile('index.html', ( err, data ) => {
+    if( err ) throw err 
+    conteudo = data
+})
+
+const server = http.createServer(( req, res ) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end( conteudo )
+   
+})
+
+server.listen(host, port, () => {
+    console.log(`Servidor rodando em ${url}`)
+})
+
+opn( url, {app: 'firefox'} )
+// 
