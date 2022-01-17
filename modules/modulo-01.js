2a7851a950f907793aaedc117719c61f56ddb107
APOLLO ALVES <apolloapolloalves@gmail.com>
2022-01-17 17:11:24 -0300

    Modules in NodeJS

---
+++ modules/modulo-01.js
@@ -0,0 +1,12 @@
+console.log('modulo-01.js is cache! ') 
+
+module.exports.oculta  = () => console.log( 'Executando função oculta' )
+module.exports.executa = () => console.log( 'Executando a função executa' )
+module.exports.welcome = 'Bem vindo ao módulo-01.js '
+
+// Módulo REPL
+// EX: DESESTRUTURAÇÃO
+module.exports.data = ( name, lastname ) =>  console.log( `Dados do usuário: ${name} ${lastname}` )
+
+const { oculta, executa, welcome  } = require( './modulo-01' )
+const { data } = require('./modulo-01')
