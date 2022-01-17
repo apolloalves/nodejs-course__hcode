023c6eff5655f4efcabe8ff67095a61df00adacd
APOLLO ALVES <apolloapolloalves@gmail.com>
2022-01-17 17:46:04 -0300

    Importanto/Exportando Modulos

--- modules/modulo-02.js
+++ modules/modulo-02.js
@@ -1,7 +1,10 @@
+// importação por desestruturação
 const { executa, oculta  } = require( './modulo-01-refact' ) 
-
-//Executando o omodulo importado.oculta()
-
-// executa()
 oculta()
+executa()
 
+
+// importação por OBJ
+const module01 = require('./modulo-01-refact')
+module01.executa()
+module01.oculta()
