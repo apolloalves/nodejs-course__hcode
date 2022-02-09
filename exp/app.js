cbd46865bce8f4e682c7458e895cb9d5b23649af
APOLLO ALVES <apolloapolloalves@gmail.com>
2022-02-09 15:16:50 -0300

    cookie-parser

--- exp/app.js
+++ exp/app.js
@@ -4,17 +4,28 @@ const app = express()
 const adminRouters = require('./routes/admin')
 const userRouters = require('./routes/users')
 const guestRouters = require('./routes/guest')
+const cookieParser = require('cookie-parser')
 
 //middleware - func
 app.use(express.json())
+app.use(cookieParser())
 
 app.use(( req, res, next) => {
     console.log('Executando midleware em nivel de app')
     return next()
 })
 
+app.get('/setcookie', ( req,res ) => {
+    res.cookie('user', 'Apollo Alves', {maxAge: 900000, httpOnly: true})
+    return res.send('Cookie gravado com sucesso!')
+})
 
+app.get('/getcookie', ( req, res ) => {
+    
+    let user = req.cookies['user']
+    if(user) return res.send(user)
 
+})
 //midlewares routes
 app.use('/admin', adminRouters)
 app.use('/users', userRouters)
--- exp/routes/users.js
+++ exp/routes/users.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-function logReq( req, res, next) {
-   console.log('Executando midleware para rota users')
-   return next()
+//func middleware
+const logReq = ( req, res, next ) => {
+    console.log('Executando midleware para rota users',)
+    return next()
 }
 
 router.get('/',logReq, ( req, res ) => res.send('acessando a rota dos usuarios'))
-
 router.put('/:id', ( req, res ) => res.send('Access from PUT'))
 router.patch('/:id', ( req, res ) => res.send('Access from PATCH'))
 router.delete('/:id', ( req,res ) => res.send('Access from DELETE'))
